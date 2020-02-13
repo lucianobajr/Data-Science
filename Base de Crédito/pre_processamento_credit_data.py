7fe280958e5924e4691fc2f8f173b749515f1a76
lucianobajr <luciano.alcantara@ufv.br>
2020-02-13 10:06:16 -0300

    att

--- Base de Crédito/pre_processamento_credit_data.py
+++ Base de Crédito/pre_processamento_credit_data.py
@@ -1,11 +1,19 @@
 '''clientid:id do cliente / income:salário ou renda(anual) /
 age:idade / loan:impréstimo /default:atributo classe/'''
-import pandas as pd
-base = pd.read_csv('credit-data.csv')
-print(base)
-print(base.describe())
-'''
-count:quantidade
+import pandas as pd  # pd é o apelido para pandas
+base = pd.read_csv('credit-data.csv')  # carregar a base csv
+print(base.describe(), "\n")  # ver algumas estatísticas
+
+'''count:quantidade
 mean:media
-std:desvio padrão
-'''
+std:desvio padrão'''
+
+# localiza as idades 'erradas' no caso negativas
+print('\n', base.loc[base['age'] < 0], '\n')
+
+'''metodos para modificar dados'''
+# apagar a coluna   base.drop('age', 1, inplace=True)
+# apagar somente os registro com problema  base.drop(base[base.age < 0].index, inplace=True)
+# preencher os valores manualmente
+# preencher os valores com a média
+base.loc[base.age < 0, 'age'] = (base['age'][base.age > 0].mean())
