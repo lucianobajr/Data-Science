65e35fbeb1257a50238d676919ca00818006e3d9
[Luciano] <[luciano.alcantara@ufv.br]>
2020-02-13 20:49:26 -0300

    Escalonamento de Atributos

--- Base de Crédito/pre_processamento_credit_data.py
+++ Base de Crédito/pre_processamento_credit_data.py
@@ -1,6 +1,10 @@
 '''clientid:id do cliente / income:salário ou renda(anual) /
 age:idade / loan:impréstimo /default:atributo classe/'''
 import pandas as pd  # pd é o apelido para pandas
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
 base = pd.read_csv('credit-data.csv')  # carregar a base csv
 print(base.describe(), "\n")  # ver algumas estatísticas
 '''count:quantidade
@@ -9,7 +13,7 @@ std:desvio padrão'''
 
 # localiza as idades 'erradas' no caso negativas
 print('\n', base.loc[base['age'] < 0], '\n')
-'''metodos para modificar dados'''
+'''metodos para modificar dados-Tratamento de valores inconsistentes'''
 # apagar a coluna
 # base.drop('age', 1, inplace=True)
 # apagar somente os registro com problema
@@ -17,7 +21,7 @@ print('\n', base.loc[base['age'] < 0], '\n')
 # preencher os valores manualmente
 # preencher os valores com a média
 #base.loc[base.age < 0, 'age'] = (base['age'][base.age > 0].mean())
-
+''' Tratamento  de valores faltantes'''
 #imprime valores não preenchidos
 pd.isnull(base['age'])
 base.loc[pd.isnull(base['age'])]
@@ -26,6 +30,9 @@ base.loc[pd.isnull(base['age'])]
 previsores = base.iloc[:, 1:4].values
 classe = base.iloc[:, 4].values
 
-from sklearn.preprocessing import Imputer
-
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
+''' Escalonamento de Atributos '''
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
