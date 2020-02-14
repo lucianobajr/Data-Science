3a71882e6e93f3baf7485ae1d5f76514c917e944
lucianobajr <luciano.alcantara@ufv.br>
2020-02-14 16:27:33 -0300

    att

--- Base_de_Credito/template-credit-data.py
+++ Base_de_Credito/template-credit-data.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
 
-base = pd.read_csv('credit_data.csv')
+base = pd.read_csv('credit-data.csv')
 base.loc[base.age < 0, 'age'] = 40.92
                
 previsores = base.iloc[:, 1:4].values
 classe = base.iloc[:, 4].values
 
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer = imputer.fit(previsores[:, 1:4])
 previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
 
@@ -15,14 +16,15 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 previsores = scaler.fit_transform(previsores)
 
-from sklearn.cross_validation import train_test_split
+''' Divisão de bases de dados'''
+from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
 
 # importação da biblioteca
 # criação do classificador
-classificador.fit(previsores_treinamento, classe_treinamento)
-previsoes = classificador.predict(previsores_teste)
+#classificador.fit(previsores_treinamento, classe_treinamento)
+#previsoes = classificador.predict(previsores_teste)
 
 from sklearn.metrics import confusion_matrix, accuracy_score
-precisao = accuracy_score(classe_teste, previsoes)
-matriz = confusion_matrix(classe_teste, previsoes)
+#precisao = accuracy_score(classe_teste, previsoes)
+#matriz = confusion_matrix(classe_teste, previsoes)
