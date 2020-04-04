627d13e9221af0f9dfa63b6aa2bfdc94f490f816
Luciano Belo <luciano.alcantara@ufv.br>
2020-04-04 10:03:48 -0300

    and module KNN

---
+++ 6.KNN/knn-credit_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+base = pd.read_csv('../data/credit-data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+               
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+''' Divisão de bases de dados'''
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+classificador = KNeighborsClassifier(n_neighbors=5,metric='minkowski', p = 2)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+print("%.1f%% de precisão "%(precisao*100))
+print(matriz)
+
+import collections
+collections.Counter(classe_teste)
